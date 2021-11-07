8a955c535096cfbfe63d064592030569f37cf3c0
Gerd Aschemann <gerd@aschemann.net>
2021-11-07 13:39:26 +0100

    Start an "empty" demo

---
+++ main.tf
@@ -0,0 +1,17 @@
+resource "aws_network_interface" "this" {
+  subnet_id = local.default_subnet_id
+}
+
+resource "aws_eip" "this" {
+  network_interface = aws_network_interface.this.id
+}
+
+resource "aws_lb" "this" {
+  name               = "LB-dummy"
+  load_balancer_type = "network"
+
+  subnet_mapping {
+    subnet_id     = local.default_subnet_id
+    allocation_id = aws_eip.this.id
+  }
+}
