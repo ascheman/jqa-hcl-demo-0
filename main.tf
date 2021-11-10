5a4d818988426c035a65c50e74e6313d29c0cd05
Gerd Aschemann <gerd@aschemann.net>
2021-11-10 23:48:02 +0100

    Drop superfluous network interface

--- main.tf
+++ main.tf
@@ -1,9 +1,4 @@
-resource "aws_network_interface" "this" {
-  subnet_id = local.default_subnet_id
-}
-
 resource "aws_eip" "this" {
-  network_interface = aws_network_interface.this.id
 }
 
 resource "aws_lb" "this" {
