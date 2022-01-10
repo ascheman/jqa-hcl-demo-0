f0988ab06558192cce48fc781daf716d55279273
Gerd Aschemann <gerd@aschemann.net>
2022-01-10 07:41:23 +0100

    Add Groovy based resource report

---
+++ jqassistant/groovy/tst.groovy
@@ -0,0 +1,22 @@
+#!/usr/bin/env groovy
+
+File reportFile = new File(reportDirectory, "report.adoc")
+logger.info ("Creating report file '{}'", reportFile)
+
+reportFile.write """= ${concept.id}
+
+${concept.description}
+
+|===
+| ${result?.columnNames.join(' | ')}
+
+"""
+
+result?.rows.each {row ->
+    row.each { key, value ->
+        reportFile << "| ${value} "
+    }
+    reportFile << "\n"
+}
+
+reportFile << "|==="
